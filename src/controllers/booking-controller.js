abc3505757eee6f3271bf72c7f8a5e258e4d0c53
Ravi Kumar <ravikumar956078@gmail.com>
2023-03-18 23:30:00 +0530

    Changes to test message queue

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -12,8 +12,16 @@ class BookingController {
 
   async sendMessageToQueue(req, res) {
     const channel = await createChannel();
-    const data = { message: 'Success' };
-    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+    const payload = {
+      data: {
+        subject: 'This is a noti from queue',
+        content: 'Some queue will subscribe this',
+        recepientEmail: 'rk721084@gmail.com',
+        notificationTime: '2023-03-16T10:03:00',
+      },
+      service: 'CREATE_TICKET',
+    };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
     return res.status(200).json({
       message: 'Successfully published the event',
     });
